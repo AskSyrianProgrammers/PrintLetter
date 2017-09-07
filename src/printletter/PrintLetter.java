a66981946ebc0595dbb10bb1d618c3baf7e6cb7a
Amer Harb <amer.harb@gmail.com>
2017-09-07 15:28:55 +0200

    add proc for all letters, still need to be filled with array values

--- src/printletter/PrintLetter.java
+++ src/printletter/PrintLetter.java
@@ -122,25 +122,58 @@ public class PrintLetter {
         LP.put(' ', getLetterSpace());
         LP.put('-', getLetterDash());
         LP.put('A', getLetterA());
-        LP.put('a', getLetterA());
         LP.put('B', getLetterB());
-        LP.put('b', getLetterB());
         LP.put('C', getLetterC());
-        LP.put('c', getLetterC());
         LP.put('D', getLetterD());
-        LP.put('d', getLetterD());
         LP.put('E', getLetterE());
-        LP.put('e', getLetterE());
         LP.put('F', getLetterF());
-        LP.put('f', getLetterF());
         LP.put('G', getLetterG());
-        LP.put('g', getLetterG());
         LP.put('H', getLetterH());
-        LP.put('h', getLetterH());
         LP.put('I', getLetterI());
+        LP.put('J', getLetterJ());
+        LP.put('K', getLetterK());
+        LP.put('L', getLetterL());
+        LP.put('M', getLetterM());
+        LP.put('N', getLetterN());
+        LP.put('O', getLetterO());
+        LP.put('P', getLetterP());
+        LP.put('Q', getLetterQ());
+        LP.put('R', getLetterR());
+        LP.put('S', getLetterS());
+        LP.put('T', getLetterT());
+        LP.put('U', getLetterU());
+        LP.put('V', getLetterV());
+        LP.put('W', getLetterW());
+        LP.put('X', getLetterX());
+        LP.put('Y', getLetterY());
+        LP.put('Z', getLetterZ());
+        LP.put('a', getLettera());
+        LP.put('b', getLetterb());
+        LP.put('c', getLetterc());
+        LP.put('d', getLetterd());
+        LP.put('e', getLettere());
+        LP.put('f', getLetterf());
+        LP.put('g', getLetterg());
+        LP.put('h', getLetterh());
         LP.put('i', getLetteri());
-
-        //TODO: call the rest of the letters
+        LP.put('j', getLetterj());
+        LP.put('k', getLetterk());
+        LP.put('l', getLetterl());
+        LP.put('m', getLetterm());
+        LP.put('n', getLettern());
+        LP.put('o', getLettero());
+        LP.put('p', getLetterp());
+        LP.put('q', getLetterq());
+        LP.put('r', getLetterr());
+        LP.put('s', getLetters());
+        LP.put('t', getLettert());
+        LP.put('u', getLetteru());
+        LP.put('v', getLetterv());
+        LP.put('w', getLetterw());
+        LP.put('x', getLetterx());
+        LP.put('y', getLettery());
+        LP.put('z', getLetterz());
+        //TODO: add the new charchter 
         //....
     }
 
@@ -320,16 +353,251 @@ public class PrintLetter {
 
         boolean O = false, X = true;
         boolean L[][] = {
-                            {X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}                        };
+                            {X, X, X, X, X, O, O}
+                           ,{X, X, X, X, X, O, O}
+                           ,{O, X, X, X, O, O, O}
+                           ,{O, X, X, X, O, O, O}
+                           ,{O, X, X, X, O, O, O}
+                           ,{O, X, X, X, O, O, O}
+                           ,{O, X, X, X, O, O, O}
+                           ,{O, X, X, X, O, O, O}
+                           ,{X, X, X, X, X, O, O}
+                           ,{X, X, X, X, X, O, O}                        
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterJ() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterK() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterL() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+                            {X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, O, O, O, O, O, O, O}
+                           ,{X, X, X, X, X, X, X, X, O, O}
+                           ,{X, X, X, X, X, X, X, X, O, O}                       
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterM() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterN() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterO() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterP() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterQ() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterR() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterS() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterT() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterU() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterV() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterW() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterX() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterY() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterZ() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLettera() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterb() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterc() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterd() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLettere() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterf() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterg() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterh() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
         return L;
     }
 
@@ -346,10 +614,172 @@ public class PrintLetter {
                            ,{X, X, X, O, O}
                            ,{X, X, X, O, O}
                            ,{X, X, X, O, O}
-                           ,{X, X, X, O, O}                        };
+                           ,{X, X, X, O, O}                        
+        };
         return L;
     }
 
+    private static boolean[][] getLetterj() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterk() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterl() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+                            {X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}
+                           ,{X, X, X, O, O}                       
+                        };
+        return L;
+    }
+
+    private static boolean[][] getLetterm() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLettern() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLettero() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterp() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterq() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterr() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetters() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLettert() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetteru() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterv() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterw() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterx() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLettery() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
+
+    private static boolean[][] getLetterz() {
+
+        boolean O = false, X = true;
+        boolean L[][] = {
+            {X}
+        };
+        return L;
+    }
     //TODO: do the rest letters ....X Y Z
     //...
     private static boolean[][] getLetterRandom() {
